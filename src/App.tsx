fffb80892e0019e3cc5d2a14d72ad1056201b0d0
Andrey Silva Silveira <andreysilveira.1997@gmail.com>
2021-09-19 00:08:41 -0300

    React02

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -19,8 +16,8 @@ interface GenreResponseProps {
   title: string;
 }
 
+
 interface MovieProps {
-  imdbID: string;
   Title: string;
   Poster: string;
   Ratings: Array<{
@@ -52,44 +49,19 @@ export function App() {
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
+
   }, [selectedGenreId]);
 
   function handleClickButton(id: number) {
-    setSelectedGenreId(id);
+    setSelectedGenreId(id)
   }
 
   return (
+    <>
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <SideBar handleClickButton={handleClickButton} genres={genres} selectedGenreId={selectedGenreId}/>
+      <Content selectedGenre={selectedGenre} movies={movies}/>
     </div>
+    </>
   )
 }
